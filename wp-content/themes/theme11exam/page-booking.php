197942bcd7042fb8acc7391c427693404cf3c4bc
Ting Hua Lin <coolting.lin3@gmail.com>
2023-11-27 11:08:05 +0100

    fix parse error

--- wp-content/themes/theme11exam/page-booking.php
+++ wp-content/themes/theme11exam/page-booking.php
@@ -1,8 +1,8 @@
 <?php
-<div>
-<h1>WORK GODDANM IT </h1>
-<h2>test on wppusher</h2>
-<h2>test on wppusher with webhook</h2></div>
-<script src="booking.js"/>
-
+echo '<div>
+    <h1>WORK GODDANM IT </h1>
+    <h2>test on wppusher</h2>
+    <h2>test on wppusher with webhook</h2>
+</div>';
 ?>
+<script src="booking.js"></script>
