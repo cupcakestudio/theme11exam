c56c8fffb12e0df5d6f7568a4435a22e69680f0f
Ting Hua Lin <coolting.lin3@gmail.com>
2023-11-28 13:26:45 +0100

    add some booking

--- wp-content/themes/theme11exam/page-booking.php
+++ wp-content/themes/theme11exam/page-booking.php
@@ -1,8 +1,5 @@
 <?php
-echo '<div>
-    <h1>WORK GODDANM IT </h1>
-    <h2>test on wppusher</h2>
-    <h2>test on wppusher with webhook</h2>
-</div>';
+
+include("booking.html")
 ?>
 <script src="booking.js"></script>
