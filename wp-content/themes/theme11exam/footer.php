43507ba14bd17556ea45d1ccd1684ba7b93d97f9
Ting Hua Lin <coolting.lin3@gmail.com>
2023-12-08 16:57:31 +0100

    change theme to hestia, drop booking use plugin

---
+++ wp-content/themes/theme11exam/footer.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/png" href="images/favicon_k2.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=DM+Serif+Text:400,700&display=swap">
+      <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/customheaderfooter.css ">
+</head>
+		
+    <title>footer</title>
+  <body>
+    <footer>
+      <div class="grid_fot">
+        <div class="col_1">
+          <h3>Kontakt</h3>
+          <div class="contact-info">
+            <i class="fas fa-envelope"></i>
+            <p>Fodklinik@nythaab.dk</p>
+          </div>
+          <div class="contact-info">
+            <i class="fas fa-phone"></i>
+            <p>+45 86 16 43 93</p>
+          </div>
+          <div class="contact-info">
+            <i class="fas fa-map-marker-alt"></i>
+            <p>Ivar Huitfeldtsgade 79,8200 Århus N</p>
+          </div>
+        </div>
+        <div class="col_2">
+          <h3>Åbningstider</h3>
+          <p class="exstra_text">Alle hverdage fra kl. 9:30-15:30</p>
+          <p class="exstra_text">Lukket på helligdage & i weekender</p>
+        </div>
+        <div class="col_3">
+          <h3>Nyhåb's fodklinik</h3>
+          <img src="https://thlstudios.dk/kea/theme11exam-wp/wp-content/uploads/2023/12/Bomaerke_Fodterapeuter_pos_cmyk.pdf" alt="logo" class="maerke" />
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
+<?php wp_footer(); ?>
