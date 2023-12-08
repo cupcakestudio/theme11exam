66a8fe5f2dd50238f5cffc4077d6129f4044ffd2
karoline <karo8062@stud.kea.dk>
2023-12-08 13:29:32 +0100

    done med header og footer

---
+++ wp-content/themes/theme11exam/footer.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "wrapper" div and all content after.
+ *
+ * @package Hestia
+ * @since Hestia 1.0
+ */
+include("footer.html")
---
+++ wp-content/themes/theme11exam/header.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the page header div.
+ *
+ * @package Hestia
+ * @since Hestia 1.0
+ */
+include("header.html")
