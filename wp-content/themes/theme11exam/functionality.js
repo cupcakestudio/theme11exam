5f0247538db948adfa72310b4abe9baa6a2cd5f3
Ting Hua Lin <coolting.lin3@gmail.com>
2023-12-10 11:14:20 +0100

    add pagination buttons and make them clickable
    Co-authored-by: SofiaSRokkedal <simonerok@users.noreply.github.com>

--- wp-content/themes/theme11exam/functionality.js
+++ wp-content/themes/theme11exam/functionality.js
@@ -1,35 +1,83 @@
-const slide_container = document.getElementsByClassName("slide_container");
 const slide = document.getElementsByClassName("slide");
+const allSlides = document.querySelectorAll("li");
 const prevButton = document.getElementById("prev");
 const nextButton = document.getElementById("next");
+
 const pagination_container = document.getElementsByClassName("pagination");
-
+const slides_pagination = document.getElementById("slides_pagination");
+const pagination_numbers = document.getElementById("pagination-numbers");
 let currentPage = 1; //current page
-const maxPages = 3; //max number of pagination slides
+const paginationLimit = 3; // show 3 items at one page - How many slides are shown/visible on on the current page
+const pageLength = Math.ceil(allSlides.length / paginationLimit); // Number of pages available.
 
-//handle next
+//handle next button pagination
 nextButton.addEventListener("click", () => {
   setCurrentPage(currentPage + 1);
   console.log("next", currentPage);
-  if (currentPage > maxPages) {
-    currentPage = 0;
+  //set page  to first page
+  if (currentPage >= paginationLimit) {
+    currentPage = 1;
     console.log(currentPage);
   }
 });
 
-//handle prev
+//handle prev button pagination
 prevButton.addEventListener("click", () => {
   setCurrentPage(currentPage - 1);
   console.log("prev", currentPage);
-  if (currentPage < 1) {
-    currentPage = maxPages;
+  //set page  to last page
+  if (currentPage <= paginationLimit - 2) {
+    currentPage = paginationLimit;
     console.log(currentPage);
   }
 });
 
-//when a button is pressed, change the content in carousel, with the list item
+// Function to make dots possible to click through
+function appendPagination(index) {
+  const pageNumber = document.createElement("button");
+  pageNumber.className = "pagination_dot";
+  pageNumber.innerHTML = index;
+  pageNumber.setAttribute("page_index", index); // sets an attribute to the html element
+  pageNumber.setAttribute("aria_label", "page" + index); // sets an attribute to the html element
 
+  pagination_numbers.appendChild(pageNumber);
+}
+
+// get pagination number trough a forloop function
+function getPaginationNumbers() {
+  for (let i = 1; i <= pageLength; i++) {
+    appendPagination(i);
+  }
+}
+
+//when a button is pressed, change the content in carousel, with the list item
 //set the curent page
 function setCurrentPage(pageNumber) {
   currentPage = pageNumber;
+
+  //handle when a user clicks on an arbitrary page-button (en cirkel i bunden)
+
+  // Defines a range for how many slides is shown at a time
+
+  const prevRange = (pageNumber - 1) * paginationLimit;
+  const currRange = pageNumber * paginationLimit;
+
+  // loop trough all slides and hide other slides
+  allSlides.forEach((item, index) => {
+    item.classList.add("hidden");
+    if (index >= prevRange && index < currRange) {
+      item.classList.remove("hidden");
+    }
+  });
+  console.log("this is current page", currentPage);
 }
+
+// waits for the page to load the content before making functionality available
+window.addEventListener("load", () => {
+  getPaginationNumbers();
+  setCurrentPage(1);
+  console.log("this is current page", currentPage);
+});
+
+// makes pagination dots clickable
+/* document.querySelectorAll(""); */
