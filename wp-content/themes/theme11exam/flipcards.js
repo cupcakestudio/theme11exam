57f6f302893a47b8ea771ea9fdfd2064ff5c5595
Simone <ssimone12@gmail.com>
2023-12-12 14:12:13 +0100

    flip back now works

--- wp-content/themes/theme11exam/flipcards.js
+++ wp-content/themes/theme11exam/flipcards.js
@@ -1,19 +1,13 @@
-window.addEventListener("DOMContentLoaded", () => flipAnimationBtn());
+window.addEventListener("DOMContentLoaded", () => {
+  flipAnimationBtn();
+  flipBack(); // Call the flipBack function to set up the click event for flip back
+});
+
 const cards = document.querySelectorAll(".slide");
 
 function flipCard(card) {
   card.classList.toggle("flipped");
   console.log("toggle flipped");
-  // if ("flipped") {
-  //   // const hiddenContent = card.querySelector(".back");
-
-  //   // hiddenContent.style.display =
-  //   //   hiddenContent.style.display === "none" ? "none" : "flex";
-
-  // }
-  // if (!"flipped") {
-  //   console.log("back to front");
-  // }
 }
 
 function flipAnimationBtn() {
@@ -21,11 +15,25 @@ function flipAnimationBtn() {
   console.log(slideBtn);
   slideBtn.forEach((button) =>
     button.addEventListener("click", () => {
-      console.log("clicked");
+      /* console.log("clicked"); */
       const slide = button.closest(".slide");
       if (slide) {
         flipCard(slide);
       }
     })
   );
+}
+
+//flip back
+function flipBack() {
+  const back_arrows = document.querySelectorAll(".arrow_backside");
+  back_arrows.forEach((button) => {
+    button.addEventListener("click", () => {
+      console.log("flip back");
+      const slide = button.closest(".slide");
+      if (slide) {
+        flipCard(slide);
+      }
+    });
+  });
 }
